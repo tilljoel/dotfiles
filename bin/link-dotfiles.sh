7b2d881a32f5485dac148ec0f43e27f442e07ee7
Joel Larsson <tilljoel@gmail.com>
2013-05-21 01:02:17 +0200

    Fix script so it works
    Now it creates correct links

--- bin/link-dotfiles.sh
+++ bin/link-dotfiles.sh
@@ -1,22 +1,30 @@
 #!/bin/sh
-#DOTFILES_DIR=`dirname $(greadlink -f $0)`
 cd /Users/joel/.dotfiles
+
 echo `pwd`
-for file in `git ls-files`; do
-    file=`greadlink -f $file`
-    echo "link $file"
-    ln -sf $file $HOME
+for file in `git ls-tree --name-only HEAD ./`; do
+  FILE=`greadlink -f $file`
+
+  case "$FILE" in
+    *dotfiles/bin*) ln -sf $FILE $HOME/bin ;;
+    *)              ln -sf $FILE $HOME ;;
+  esac
+    echo "ln -sf $file $HOME"
+    #ln -sf $file $HOME
 done
 
 shopt -s dotglob
+
 cd /Users/joel/.dotfiles_secret
 echo `pwd`
-for file in * 
+for file in *
 do
-    if test -f "$file" 
-    then
-       echo "link $file"
-       ln -sf $file $HOME
-    fi
+  if test -f "$file"
+  then
+    FILE=`greadlink -f $file`
+    echo "ln -sf $FILE $HOME"
+    ln -sf $FILE $HOME
+  fi
 done
+
 shopt -u dotglob
