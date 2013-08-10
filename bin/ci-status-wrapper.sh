13d8ed0bdf0b310818e4e8542846b4e1227729c1
Joel Larsson <tilljoel@gmail.com>
2013-08-10 09:26:51 +0200

    Update ci-status/powerline

--- bin/ci-status-wrapper.sh
+++ bin/ci-status-wrapper.sh
@@ -2,18 +2,11 @@ path=$(git rev-parse --show-cdup)
 cache_file="./$path.ci-status"
 git_dir="./$path.git"
 
-echo $path
-echo $cache_file
-echo $git_dir
 if [ -e $cache_file ]; then
-  if [ $cache_file -nt $git_dir ]; then
-    echo 'cache'
-    cat $cache_file
-  else
-    echo 'update cache'
-    ci-status --reporter=simple > $cache_file
+  if [ $cache_file -ot $git_dir ]; then
+    ci-status --reporter=simple | cut -f 5 -d' ' > $cache_file
   fi
 else
-  echo 'first live'
-  ci-status --reporter=simple > $cache_file
+  ci-status --reporter=simple | cut -f 5 -d' ' > $cache_file
 fi
+cat $cache_file
