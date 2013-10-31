199bfcc95202da73c6f7035706bd551bedadd1bc
Joel Larsson <tilljoel@gmail.com>
2013-11-01 00:18:06 +0100

    Prompt: fix powerline and bs

--- bin/ci-status-wrapper.sh
+++ bin/ci-status-wrapper.sh
@@ -1,12 +1,12 @@
 path=$(git rev-parse --show-cdup)
-cache_file="./$path.ci-status"
+cache_file="./$path.bs"
 git_dir="./$path.git"
 
 if [ -e $cache_file ]; then
   if [ $cache_file -ot $git_dir ]; then
-    ci-status --reporter=simple | cut -f 5 -d' ' > $cache_file
+    (RBENV_VERSION=1.9.3 ci-status --reporter=minimal > $cache_file &)
   fi
 else
-  ci-status --reporter=simple | cut -f 5 -d' ' > $cache_file
+  (RBENV_VERSION=1.9.3 ci-status --reporter=minimal > $cache_file &)
 fi
 cat $cache_file
